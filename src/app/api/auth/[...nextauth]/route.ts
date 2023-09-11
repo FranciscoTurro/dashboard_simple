22b86ccf1315c02ff397a537a06040e3cc3f0155
FranciscoTurro <franciscogtrro@outlook.com.ar>
2023-09-11 20:41:51 -0300

    replaced yarn with pnpm, fixed build issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,37 +1,6 @@
 import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { db } from '../../../../lib/db';
-import { users } from '../../../../lib/db/schema';
-import { eq } from 'drizzle-orm';
-import argon2 from 'argon2';
+import authOptions from '../../../../lib/auth/authOptions';
 
-export const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        user_name: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      authorize: async (credentials) => {
-        if (!credentials) return null;
-        const user = await db.query.users.findFirst({
-          where: eq(users.user_name, credentials.user_name),
-        });
-
-        if (!user) return null;
-
-        const validPass = await argon2.verify(
-          user.password!, //puedo garantizar que la password existe porque el usuario existe
-          credentials.password
-        );
-
-        if (!validPass) return null;
-
-        return { id: user.user_id, name: user.user_name } as any; //as any :c bug de next-auth
-      },
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
